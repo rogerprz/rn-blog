396c4dd6ff762e563705b364ac91e692103e6c79
Roger Perez <ropere@microsoft.com>
2022-03-23 12:47:51 -0700

    Fix typo and update reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,26 +1,19 @@
-import React, { useReducer } from 'react';
-
-const BlogContext = React.createContext();
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_BLOG_POST':
-      return [...state, { title: action.payload.title, content: action.payload.content, id: Math.random() }];
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
     case 'DELETE_BLOG_POST':
       return state.filter((blogPost) => blogPost.id !== action.payload);
     default:
       return state;
   }
 };
-
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, [
-    { title: 'Initial title', content: 'Initial Content', id: 1 }
-  ]);
-  const addBlogPost = (title, content) => {
-    dispatch({ type: 'ADD_BLOG_POST', payload: { title, content } });
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: 'ADD_BLOG_POST' });
   };
-  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>{children}</BlogContext.Provider>;
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,21 +1,21 @@
 import React, { useContext } from 'react';
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { Context } from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(Context);
+
   return (
     <View>
       <Text>Index Screen</Text>
-      <Button title="Add Post" onPress={() => addBlogPost('Title', 'Content')} />
+      <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
-        data={data}
-        keyExtractor={(blogPost) => blogPost.id}
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return (
             <View>
               <Text>{item.title}</Text>
-              <Text>{item.content}</Text>
             </View>
           );
         }}
