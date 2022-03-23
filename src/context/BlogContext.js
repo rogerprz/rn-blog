c582bc598138e194925d6857d8cc2a657220e7af
Roger Perez <ropere@microsoft.com>
2022-03-23 12:17:09 -0700

    Add reducer function + payload

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,26 +1,25 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_BLOG_POST':
+      return [...state, { title: action.payload.title, content: action.payload.content, id: Math.random() }];
+    case 'DELETE_BLOG_POST':
+      return state.filter((blogPost) => blogPost.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([
-    {
-      id: '1',
-      title: 'Blog Post 1',
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
-    },
-    {
-      id: '2',
-      title: 'Blog Post 2',
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
-    }
+  const [blogPosts, dispatch] = useReducer(blogReducer, [
+    { title: 'Initial title', content: 'Initial Content', id: 1 }
   ]);
-
   const addBlogPost = (title, content) => {
-    title = `Blog post #${blogPosts.length + 1}`;
-    setBlogPosts([...blogPosts, { id: blogPosts.length + 1, title, content }]);
+    dispatch({ type: 'ADD_BLOG_POST', payload: { title, content } });
   };
-
   return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>{children}</BlogContext.Provider>;
 };
 
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
@@ -7,6 +7,7 @@ const IndexScreen = () => {
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={() => addBlogPost('Title', 'Content')} />
       <FlatList
         data={data}
         keyExtractor={(blogPost) => blogPost.id}
