0de622799fb7b4af218cf55e78bb54f740b64329
Roger Perez <ropere@microsoft.com>
2022-03-24 14:03:50 -0700

    add text and textinput

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -5,7 +5,7 @@ import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(Context);
-
+  console.log('WE here');
   return (
     <View>
       <Text>Index Screen</Text>
@@ -30,6 +30,16 @@ const IndexScreen = ({ navigation }) => {
       />
     </View>
   );
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    )
+  };
 };
 
 const styles = StyleSheet.create({
